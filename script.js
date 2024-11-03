cd6c976987308e567a7cfbd70c4d6a4a8e802643
M1K3Y-917 <okudilimicheal@gmail.com>
2024-11-03 13:54:45 +0100

    map(img) added

--- script.js
+++ script.js
@@ -51,3 +51,15 @@ const naav = new IntersectionObserver(
 );
 
 naav.observe(abt);
+
+// var map = L.map("map").setView([6.6225373, 3.2834476], 20);
+
+// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// L.marker([6.6225373, 3.2834476])
+//   .addTo(map)
+//   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+//   .openPopup();
